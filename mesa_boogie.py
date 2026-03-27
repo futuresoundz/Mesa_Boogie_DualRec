9b6f474b6a96eb56178cef34390d1f3c83bac51b
futuresoundz <271075570+futuresoundz@users.noreply.github.com>
2026-03-26 23:11:24 -0700

    Update mesa_boogie.py

--- mesa_boogie.py
+++ mesa_boogie.py
@@ -1,21 +1,25 @@
 import numpy as np
 import scipy.io.wavfile as wav
 
-file = 'clean_guitar.wav'
-rate, data = wav.read(file)
+file_path = 'mysound.wav'
+rate, data = wav.read(file_path)
 
 volume_boost = 20 
 distorted = data * volume_boost
 
 
-for i in range(len(distorted)):
-    if distorted[i] > 15000:
-        distorted[i] = 15000
-    if distorted[i] < -15000:
-        distorted[i] = -15000
+#for i in range(len(distorted)):
+   # if distorted[i] > 15000:
+      #  distorted[i] = 15000
+    #if distorted[i] < -15000:
+       # distorted[i] = -15000
 
+distorted = np.clip(distorted, -15000, 15000)
 
-final_sound = distorted + (data * 2)
+window_size=20
+filtered = np.convolve(distorted, np.ones(window_size)/window_size, mode = 'same')
+
+final_sound = filtered + (data * 2)
 
 
 final_sound = final_sound * 0.5
@@ -23,4 +27,4 @@ final_sound = final_sound * 0.5
 
 out = final_sound.astype(np.int16)
 
-wav.write('BOOGIE_DISTORTION_V1.wav', rate, out)
+wav.write('DISTORTION.wav', rate, out)
